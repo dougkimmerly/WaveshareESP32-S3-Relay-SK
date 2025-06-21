23136bc072320cc857b7c397637ba7bbeee32dc8
Jan <info@johnyseven.cz>
2025-06-21 23:06:16 +0200

    Initial working version.

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,58 @@
 #include <memory>
 
 #include "sensesp.h"
+#include "sensesp/controllers/smart_switch_controller.h"
 #include "sensesp/sensors/analog_input.h"
 #include "sensesp/sensors/digital_input.h"
+#include "sensesp/sensors/digital_output.h"
 #include "sensesp/sensors/sensor.h"
 #include "sensesp/signalk/signalk_output.h"
+#include "sensesp/signalk/signalk_put_request_listener.h"
 #include "sensesp/system/lambda_consumer.h"
+#include "sensesp/transforms/repeat.h"
 #include "sensesp_app_builder.h"
 
 using namespace sensesp;
+
+// relay GPIO pins
+const uint8_t kRelayPin1 = 1;
+const uint8_t kRelayPin2 = 2;
+const uint8_t kRelayPin3 = 41;
+const uint8_t kRelayPin4 = 42;
+const uint8_t kRelayPin5 = 45;
+const uint8_t kRelayPin6 = 46;
+const uint8_t kBuzzerPin = 21;
+const uint8_t kRGBLedPin = 38;
+
+SmartSwitchController* initialize_relay(uint8_t pin, String sk_path,
+                                        String config_path_sk_output) {
+  // Initialize the relay pin to output
+  pinMode(pin, OUTPUT);
+  // Set the relay GPIO pins to LOW (off) initially
+  digitalWrite(pin, LOW);
+  auto* load_switch = new DigitalOutput(pin);
+
+  // Create a switch controller to handle the user press logic and
+  // connect it to the load switch...
+  SmartSwitchController* controller = new SmartSwitchController(true);
+  controller->connect_to(load_switch);
+
+  // In addition to the manual button "click types", a
+  // SmartSwitchController accepts explicit state settings via
+  // any boolean producer as well as any "truth" values in human readable
+  // format via a String producer.
+  // Here, we set up a SignalK PUT request listener to handle
+  // requests made to the Signal K server to set the switch state.
+  // This allows any device on the SignalK network that can make
+  // such a request to also control the state of our switch.
+  auto* sk_listener = new StringSKPutRequestListener(sk_path);
+  sk_listener->connect_to(controller->truthy_string_consumer_);
+
+  load_switch->connect_to(new Repeat<bool, bool>(10000))
+      ->connect_to(new SKOutputBool(sk_path, config_path_sk_output));
+
+  return controller;
+}
 
 // The setup function performs one-time application initialization.
 void setup() {
@@ -28,117 +72,33 @@ void setup() {
   SensESPAppBuilder builder;
   sensesp_app = (&builder)
                     // Set a custom hostname for the app.
-                    ->set_hostname("my-sensesp-project")
+                    ->set_hostname("sensesp-relay6")
                     // Optionally, hard-code the WiFi and Signal K server
                     // settings. This is normally not needed.
                     //->set_wifi_client("My WiFi SSID", "my_wifi_password")
                     //->set_wifi_access_point("My AP SSID", "my_ap_password")
                     //->set_sk_server("192.168.10.3", 80)
                     ->get_app();
+                    
+  // write up everything to Signal K
+  auto relay_controller1 = initialize_relay(kRelayPin1, "electrical.switches.relay1.state",
+                   "sensesp-relay1");
 
-  // GPIO number to use for the analog input
-  const uint8_t kAnalogInputPin = 36;
-  // Define how often (in milliseconds) new samples are acquired
-  const unsigned int kAnalogInputReadInterval = 500;
-  // Define the produced value at the maximum input voltage (3.3V).
-  // A value of 3.3 gives output equal to the input voltage.
-  const float kAnalogInputScale = 3.3;
+  auto relay_controller2 = initialize_relay(kRelayPin2, "electrical.switches.relay2.state",
+                   "sensesp-relay2");
+  auto relay_controller3 = initialize_relay(kRelayPin3, "electrical.switches.relay3.state",
+                   "sensesp-relay3");
+  auto relay_controller4 = initialize_relay(kRelayPin4, "electrical.switches.relay4.state",
+                   "sensesp-relay4");
+  auto relay_controller5 = initialize_relay(kRelayPin5, "electrical.switches.relay5.state",
+                   "sensesp-relay5");
+  auto relay_controller6 = initialize_relay(kRelayPin6, "electrical.switches.relay6.state",
+                   "sensesp-relay6");
 
-  // Create a new Analog Input Sensor that reads an analog input pin
-  // periodically.
-  auto analog_input = std::make_shared<AnalogInput>(
-      kAnalogInputPin, kAnalogInputReadInterval, "", kAnalogInputScale);
-
-  // Add an observer that prints out the current value of the analog input
-  // every time it changes.
-  analog_input->attach([analog_input]() {
-    debugD("Analog input value: %f", analog_input->get());
-  });
-
-  // Set GPIO pin 15 to output and toggle it every 650 ms
-
-  const uint8_t kDigitalOutputPin = 15;
-  const unsigned int kDigitalOutputInterval = 650;
-  pinMode(kDigitalOutputPin, OUTPUT);
-  event_loop()->onRepeat(kDigitalOutputInterval, [kDigitalOutputPin]() {
-    digitalWrite(kDigitalOutputPin, !digitalRead(kDigitalOutputPin));
-  });
-
-  // Read GPIO 14 every time it changes
-
-  const uint8_t kDigitalInput1Pin = 14;
-  auto digital_input1 = std::make_shared<DigitalInputChange>(
-      kDigitalInput1Pin, INPUT_PULLUP, CHANGE);
-
-  // Connect the digital input to a lambda consumer that prints out the
-  // value every time it changes.
-
-  // Test this yourself by connecting pin 15 to pin 14 with a jumper wire and
-  // see if the value changes!
-
-  auto digital_input1_consumer = std::make_shared<LambdaConsumer<bool>>(
-      [](bool input) { debugD("Digital input value changed: %d", input); });
-
-  digital_input1->connect_to(digital_input1_consumer);
-
-  // Create another digital input, this time with RepeatSensor. This approach
-  // can be used to connect external sensor library to SensESP!
-
-  const uint8_t kDigitalInput2Pin = 13;
-  const unsigned int kDigitalInput2Interval = 1000;
-
-  // Configure the pin. Replace this with your custom library initialization
-  // code!
-  pinMode(kDigitalInput2Pin, INPUT_PULLUP);
-
-  // Define a new RepeatSensor that reads the pin every 100 ms.
-  // Replace the lambda function internals with the input routine of your custom
-  // library.
-
-  // Again, test this yourself by connecting pin 15 to pin 13 with a jumper
-  // wire and see if the value changes!
-
-  auto digital_input2 = std::make_shared<RepeatSensor<bool>>(
-      kDigitalInput2Interval,
-      [kDigitalInput2Pin]() { return digitalRead(kDigitalInput2Pin); });
-
-  // Connect the analog input to Signal K output. This will publish the
-  // analog input value to the Signal K server every time it changes.
-  auto aiv_metadata = std::make_shared<SKMetadata>("V", "Analog input voltage");
-  auto aiv_sk_output = std::make_shared<SKOutput<float>>(
-      "sensors.analog_input.voltage",   // Signal K path
-      "/Sensors/Analog Input/Voltage",  // configuration path, used in the
-                                        // web UI and for storing the
-                                        // configuration
-      aiv_metadata
-  );
-
-  ConfigItem(aiv_sk_output)
-      ->set_title("Analog Input Voltage SK Output Path")
-      ->set_description("The SK path to publish the analog input voltage")
-      ->set_sort_order(100);
-
-  analog_input->connect_to(aiv_sk_output);
-
-  // Connect digital input 2 to Signal K output.
-  auto di2_metadata = std::make_shared<SKMetadata>("", "Digital input 2 value");
-  auto di2_sk_output = std::make_shared<SKOutput<bool>>(
-      "sensors.digital_input2.value",    // Signal K path
-      "/Sensors/Digital Input 2/Value",  // configuration path
-      di2_metadata
-  );
-
-  ConfigItem(di2_sk_output)
-      ->set_title("Digital Input 2 SK Output Path")
-      ->set_sort_order(200);
-
-  digital_input2->connect_to(di2_sk_output);
-
-  // To avoid garbage collecting all shared pointers created in setup(),
-  // loop from here.
-  while (true) {
-    loop();
-  }
+    while(true)
+    {
+      loop();
+    }
 }
 
 void loop() { event_loop()->tick(); }
